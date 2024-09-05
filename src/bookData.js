193a6923d48a55b5fc1918ca5674a4c14cda341c
Nathalia Machado <nathyrezendemachado@gmail.com>
2024-09-05 10:42:57 -0300

    add objetos em js

---
+++ src/bookData.js
@@ -0,0 +1,96 @@
+const books = [
+  {
+    title: "O Pequeno Príncipe",
+    synopsis: "Um piloto cai no deserto e encontra um menino misterioso vindo de outro planeta.",
+    pages: 96,
+    gender: "Literatura infantojuvenil",
+    author: "Antoine de Saint-Exupéry"
+  },
+  {
+    title: "1984",
+    synopsis: "Em um futuro distópico, um homem luta contra um regime totalitário.",
+    pages: 328,
+    gender: "Ficção científica",
+    author: "George Orwell"
+  },
+  {
+    title: "Orgulho e Preconceito",
+    synopsis: "Uma história de amor e preconceito social na Inglaterra do século XIX.",
+    pages: 384,
+    gender: "Romance",
+    author: "Jane Austen"
+  },
+  {
+    title: "O Senhor dos Anéis",
+    synopsis: "Uma jornada épica para destruir um anel que ameaça o mundo.",
+    pages: 1178,
+    gender: "Fantasia",
+    author: "J.R.R. Tolkien"
+  },
+  {
+    title: "Dom Casmurro",
+    synopsis: "Um clássico da literatura brasileira que explora os ciúmes e as dúvidas de um marido.",
+    pages: 320,
+    gender: "Realismo",
+    author: "Machado de Assis"
+  },
+  {
+    title: "O Nome do Vento",
+    synopsis: "Um jovem músico com poderes mágicos embarca em uma jornada épica.",
+    pages: 992,
+    gender: "Fantasia",
+    author: "Patrick Rothfuss"
+  },
+  {
+    title: "A Metamorfose",
+    synopsis: "Um homem acorda transformado em um inseto e enfrenta uma profunda crise existencial.",
+    pages: 120,
+    gender: "Realismo mágico",
+    author: "Franz Kafka"
+  },
+  {
+    title: "O Alquimista",
+    synopsis: "Um pastor andaluz segue seus sonhos em uma jornada repleta de aventuras.",
+    pages: 192,
+    gender: "Literatura de autoajuda",
+    author: "Paulo Coelho"
+  },
+  {
+    title: "A Divina Comédia",
+    synopsis: "Uma jornada épica através do Inferno, Purgatório e Paraíso.",
+    pages: 448,
+    gender: "Poesia épica",
+    author: "Dante Alighieri"
+  },
+  {
+    title: "O Hobbit",
+    synopsis: "Um hobbit se junta a um grupo de anões em uma aventura para recuperar um tesouro.",
+    pages: 310,
+    author: "J.R.R. Tolkien",
+    gender: "Fantasia"
+  }
+];
+
+// let books = [
+//   {
+//     title: "A menina que roubava livros",
+//     synopsis: `A trajetória de Liesel Meminger é contada por uma narradora mórbida, surpreendentemente simpática. Ao perceber que a pequena ladra de livros lhe escapa, a Morte afeiçoa-se à menina e rastreia suas pegadas de 1939 a 1943.
+//                 Traços de uma sobrevivente: a mãe comunista, perseguida pelo nazismo, envia Liesel e o irmão para o subúrbio pobre de uma cidade alemã, onde um casal se dispõe a adotá-los por dinheiro. O garoto morre no trajeto e é enterrado por um coveiro que deixa cair um livro na neve. É o primeiro de uma série que a menina vai surrupiar ao longo dos anos. O único vínculo com a família é esta obra, que ela ainda não sabe ler.
+//                 Assombrada por pesadelos, ela compensa o medo e a solidão das noites com a conivência do pai adotivo, um pintor de parede bonachão que lhe dá lições de leitura. Alfabetizada sob vistas grossas da madrasta, Liesel canaliza urgências para a literatura. Em tempos de livros incendiados, ela os furta, ou os lê na biblioteca do prefeito da cidade.
+//                 A vida ao redor é a pseudo-realidade criada em torno do culto a Hitler na Segunda Guerra. Ela assiste à eufórica celebração do aniversário do Führer pela vizinhança. Teme a dona da loja da esquina, colaboradora do Terceiro Reich. Faz amizade com um garoto obrigado a integrar a Juventude Hitlerista. E ajuda o pai a esconder no porão um judeu que escreve livros artesanais para contar a sua parte naquela História. A Morte, perplexa diante da violência humana, dá um tom leve e divertido à narrativa deste duro confronto entre a infância perdida e a crueldade do mundo adulto, um sucesso absoluto - e raro - de crítica e público.`,
+//     pages: "480",
+//     gender: "Romance, Ficção juvenil, Ficção histórica e Bildungsroman.",
+//     author: "Markus Zusak",
+//     image: "./images/ .jpg"
+//   },
+//   {
+//     title: "Aurora: O despertar da mulher exausta",
+//     synopsis: `Entre paranoias, medos, desejos e ânsias do dia a dia, a criadora da comunidade Obvious, Marcela Ceribelli, convida mulheres a se libertarem de amarras sociais e refletirem sobre tudo aquilo que as impede de encontrarem sua melhor versão e serem felizes.
+//                 Acordar preocupada com as tarefas do dia. Otimizar a ida ao trabalho ouvindo notícias ou respondendo e-mails. Almoçar correndo pra ganhar tempo.
+//                 E tudo isso sendo linda, bem-sucedida e gentil. Sorridente.`,
+//     pages: "288",
+//     gender: "Autoajuda",
+//     author: "Marcela Ceribelli",
+//     image: "./images/aurora.jpg"
+//   }
+// ]
